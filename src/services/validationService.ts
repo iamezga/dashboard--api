fcc8bec386134de74eec7c9c7253c5222d2605dc
Ezequiel Garay <ezemgaray@gmail.com>
2025-07-30 22:04:34 +0200

    feat: add ValidationService

---
+++ src/services/validationService.ts
@@ -0,0 +1,39 @@
+import Validator, {
+	AsyncCheckFunction,
+	SyncCheckFunction,
+	ValidationSchema,
+	ValidatorConstructorOptions
+} from 'fastest-validator'
+
+export class ValidationService extends Validator {
+	private _cache: Record<string, SyncCheckFunction | AsyncCheckFunction> = {}
+
+	constructor(options?: ValidatorConstructorOptions) {
+		super({
+			...options,
+			useNewCustomCheckerFunction: true // using new version
+		})
+	}
+
+	compile(schema: ValidationSchema | ValidationSchema[]) {
+		const key = JSON.stringify(schema)
+		if (!this._cache[key]) {
+			this._cache[key] = super.compile(schema)
+		}
+		return this._cache[key]
+	}
+
+	async validate(
+		value: any,
+		schema: ValidationSchema,
+		meta: { [key: string]: any } = {}
+	) {
+		const check = this.compile({ $$async: true, $$strict: true, ...schema })
+		const errors = await check(value, { meta: { ...meta, validator: this } })
+		return Array.isArray(errors) ? errors : []
+	}
+}
+
+const validator = new ValidationService()
+
+export { validator }
