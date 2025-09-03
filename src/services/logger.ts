ac8bdc44d984486bcb5415f2bea3a1e87ba80971
Ezequiel Garay <ezemgaray@gmail.com>
2025-09-03 02:46:46 +0200

    feat(logger): integrate pino logger with sentry using transport
    - Install `pino-sentry-transport`
    - refactor logger service to implements sentry transporter
    - Add `*.rdb` into gitignore
    - Upgrade `sentry/node` and `sentry/profiling-node`to v10.8.0

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,20 +1,35 @@
 import config from '@/services/config'
 import pino from 'pino'
 
-const pinoConfig: pino.LoggerOptions = {
-	level: config.get('env') === 'production' ? 'info' : 'debug',
+const isDev = ['development', 'debug', 'test'].includes(config.get('env'))
 
-	transport:
-		config.get('env') === 'development' || config.get('env') === 'test'
-			? {
-					target: 'pino-pretty',
-					options: {
-						colorize: true,
-						translateTime: 'SYS:HH:MM:ss Z',
-						ignore: 'pid,hostname'
-					}
-			  }
-			: undefined,
+const pinoConfig: pino.LoggerOptions = {
+	level: isDev ? 'debug' : 'info',
+	transport: isDev
+		? {
+				target: 'pino-pretty',
+				options: {
+					colorize: true,
+					translateTime: 'SYS:HH:MM:ss Z',
+					ignore: 'pid,hostname'
+				}
+		  }
+		: config.get('sentry.dsn')
+		? {
+				target: 'pino-sentry-transport',
+				options: {
+					sentry: {
+						dsn: config.get('sentry.dsn'),
+						environment: config.get('env')
+					},
+					withLogRecord: true,
+					tags: ['level'],
+					context: ['hostname'],
+					minLevel: 40, // Only error and  fatal
+					expectPinoConfig: true
+				}
+		  }
+		: undefined,
 
 	base: {
 		app: config.get('appName')
